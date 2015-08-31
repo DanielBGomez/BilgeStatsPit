19cb54b3204d702fe4807bf5cf6595e27bf2a190
Daniel B. Gomez <cesardaniel_1997@hotmail.com>
2015-08-31 01:57:57 -0500

    Updated BilgeStatsPit 0.8

---
+++ common/footer.php
@@ -0,0 +1,6 @@
+		<div id="footer">
+			Bilge Stats Pit isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends.<br>
+			League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends Riot Games, Inc.
+		</div>
+	</body>
+</html>
